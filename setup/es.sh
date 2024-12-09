5e5e267e92a5bc4036d45fba62d9e4fb1d2b7009
Javier Alonso <alonsoquinterovizcarra@gmail.com>
2024-12-09 15:00:18 -0700

    Update es.sh
    Also improved the es.sh code a little bit.

--- setup/es.sh
+++ setup/es.sh
@@ -21,108 +21,46 @@ case $ext in
 ext="png"
 mkdir -p extracted_Emojis
 echo "Formato de salida elegido: PNG"
-echo "" # It's a separator, don't remove this line or add text here, please, ok?
-echo "Which folder do you want save extracted emoji?"
-echo "1. Default Folder"
-echo "2. Custom Folder"
-read -p "> " dircustom
-
-case $dircustom in
-"1")
-dircustom="./extracted_Emojis"
-echo "the emoji will be saved at default folder"
-;;
-"2")
-echo "Provide the path to custom folder"
-read -p "> " dircustom
-echo "The emoji will be saved at custom folder"
-;;
-esac
-
-
-curl -L --output $dircustom/extracted_$emoji_id.$ext https://cdn.discordapp.com/$emoji_id.$ext
-echo "Emoji extraído con éxito"
 ;;
 "2")
 ext="jpg"
 mkdir -p extracted_Emojis
 echo "Formato de salida elegido: JPG"
-echo "" # It's a separator, don't remove this line or add text here, please, ok?
-echo "Which folder do you want save extracted emoji?"
-echo "1. Default Folder"
-echo "2. Custom Folder"
-read -p "> " dircustom
-
-case $dircustom in
-"1")
-dircustom="./extracted_Emojis"
-echo "the emoji will be saved at default folder"
-;;
-"2")
-echo "Provide the path to custom folder"
-read -p "> " dircustom
-echo "The emoji will be saved at custom folder"
-;;
-esac
-
-
-curl -L --output $dircustom/extracted_$emoji_id.$ext https://cdn.discordapp.com/$emoji_id.$ext
-echo "Emoji extraído con éxito"
 ;;
 "3")
 ext="gif"
 mkdir -p extracted_Emojis
 echo "Formato de salida elegido: GIF"
-echo "" # It's a separator, don't remove this line or add text here, please, ok?
-echo "Which folder do you want save extracted emoji?"
-echo "1. Default Folder"
-echo "2. Custom Folder"
-read -p "> " dircustom
-
-case $dircustom in
-"1")
-dircustom="./extracted_Emojis"
-echo "the emoji will be saved at default folder"
-;;
-"2")
-echo "Provide the path to custom folder"
-read -p "> " dircustom
-echo "The emoji will be saved at custom folder"
-;;
-esac
-
-
-curl -L --output $dircustom/extracted_$emoji_id.$ext https://cdn.discordapp.com/$emoji_id.$ext
-echo "Emoji extraído con éxito"
 ;;
 "4")
 ext="webp"
 mkdir -p extracted_Emojis
 echo "Formato de salida elegido: WEBP"
-echo "" # It's a separator, don't remove this line or add text here, please, ok?
-echo "Which folder do you want save extracted emoji?"
-echo "1. Default Folder"
-echo "2. Custom Folder"
-read -p "> " dircustom
-
-case $dircustom in
-"1")
-dircustom="./extracted_Emojis"
-echo "the emoji will be saved at default folder"
-;;
-"2")
-echo "Provide the path to custom folder"
-read -p "> " dircustom
-echo "The emoji will be saved at custom folder"
-;;
-esac
-
-
-curl -L --output $dircustom/extracted_$emoji_id.$ext https://cdn.discordapp.com/$emoji_id.$ext
-echo "Emoji extraído exitosamente"
 ;;
 *) 
 echo "Opción inválida"
 exit 1
 ;;
 esac
+
+echo "" # It's a separator, don't remove this line or add text here, please, ok?
+echo "¿En que carpeta deseas guardar el emoji extraído?"
+echo "1. Carpeta predeterminada (./extracted_Emojis)"
+echo "2. Carpeta personalizada"
+read -p "> " dircustom
+
+case $dircustom in
+"1")
+dircustom="./extracted_Emojis"
+echo "El emoji se guardará en la carpeta predeterminada"
+;;
+"2")
+echo "Ingresa la ruta a la carpeta personalizada"
+read -p "> " dircustom
+echo "El emoji se guardará en la carpeta personalizada"
+;;
+esac
+
+
+curl -L --output $dircustom/extracted_$emoji_id.$ext https://cdn.discordapp.com/emojis/$emoji_id.$ext
+echo "Emoji extraído con éxito"
